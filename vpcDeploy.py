70ed57254892fa80d6fc201b7825d31a49051db6
kawangwong <nycazn724@gmail.com>
2021-09-17 17:31:31 -0400

    Add files via upload

---
+++ vpcDeploy.py
@@ -0,0 +1,104 @@
+import boto3
+
+
+client = boto3.client('ec2', region_name ='us-west-1')
+ec2 = boto3.resource('ec2', region_name ='us-west-1')
+# aws_access_key_id='AWS_ACCESS_KEY_ID', aws_secret_access_key='AWS_SECRET_ACCESS_KEY' parameters needed to run.
+
+
+vpcName = "ProductionENVtest"
+cidr_block = "192.168.1.0/24"
+#easy way to allocate values to the VPC
+
+vpc = ec2.create_vpc(CidrBlock=cidr_block)
+vpc.create_tags(
+    Tags = [
+        {"Key": "Name",
+        "Value": vpcName}
+        ]
+        )
+#creates the VPC
+
+vpcData = client.describe_vpcs(
+    Filters = [
+        {
+            # "cidr": cidr_block,
+            "Name": "tag:Name",
+            "Values": [vpcName],
+            },
+        ]
+    )
+
+vpcInfos = vpcData["Vpcs"]
+vpcDict = vpcInfos[0]
+vpcIDcode= (vpcDict["VpcId"])
+
+#pulls the VPCID to pass variable
+
+subnet1 = ec2.create_subnet(
+    TagSpecifications = [
+        {
+            "ResourceType": "subnet",
+            "Tags": [
+            {
+                "Key": "Name",
+                "Value": "public1"
+                }
+            ]
+        },
+    ],
+    VpcId = vpcIDcode,
+    CidrBlock ='192.168.1.0/26',
+    AvailabilityZone ='us-west-1a')
+
+subnet2 = ec2.create_subnet(
+    TagSpecifications = [
+        {
+            "ResourceType": "subnet",
+            "Tags": [
+            {
+                "Key": "Name",
+                "Value": "private1"
+                },
+            ]
+        }
+    ],
+    VpcId = vpcIDcode,
+    CidrBlock='192.168.1.64/26',
+    AvailabilityZone ='us-west-1a')
+
+subnet3 = ec2.create_subnet(
+        TagSpecifications = [
+        {
+            "ResourceType": "subnet",
+            "Tags": [
+            {
+                "Key": "Name",
+                "Value": "public2"
+                },
+            ]
+        }
+    ],
+    VpcId = vpcIDcode,
+    CidrBlock='192.168.1.128/26',
+    AvailabilityZone='us-west-1c')
+
+subnet4 = ec2.create_subnet(
+        TagSpecifications = [
+        {
+            "ResourceType": "subnet",
+            "Tags": [
+            {
+                "Key": "Name",
+                "Value": "private2"
+                },
+            ]
+        }
+    ],
+    VpcId = vpcIDcode,
+    CidrBlock = '192.168.1.192/26',
+    AvailabilityZone = 'us-west-1c')
+
+#creates 4 subnets
+
+print("script sucessful")
