eff935699f2d5672e8658561de8d7dfc2a8ca160
kawangwong <nycazn724@gmail.com>
2021-09-21 19:51:01 -0400

    Add files via upload

--- vpcDeploy.py
+++ vpcDeploy.py
@@ -5,13 +5,14 @@ client = boto3.client('ec2', region_name ='us-west-1')
 ec2 = boto3.resource('ec2', region_name ='us-west-1')
 ## aws_access_key_id='AWS_ACCESS_KEY_ID', aws_secret_access_key='AWS_SECRET_ACCESS_KEY' parameters needed to run.
 
+route_table = ec2.RouteTable("id")
 
 vpcName = "ProductionENVtest"
 cidr_block = "192.168.1.0/24"
 ##easy way to allocate values to the VPC
 
-vpc = ec2.create_vpc(CidrBlock=cidr_block)
-vpc.create_tags(
+vpc1 = ec2.create_vpc(CidrBlock=cidr_block)
+vpc1.create_tags(
     Tags = [
         {"Key": "Name",
         "Value": vpcName}
@@ -136,5 +137,12 @@ print(igIDcode)
 
 internet_gateway.attach_to_vpc(VpcId=vpcIDcode)
 ## attaches ig to the newly made VPC
+vpc = ec2.Vpc(vpcIDcode) ##what is the id?
+routetable = vpc.create_route_table()
+route1 = routetable.create_route(DestinationCidrBlock='0.0.0.0/0', GatewayId=igIDcode)
+##creates route table. need to add name tags to this.
+
+
+
 
 print("script sucessful")
